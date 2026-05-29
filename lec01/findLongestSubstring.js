a4e291ac561882a912645e253237d298bbd4c0bc
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-05-29 22:45:42 +0200

    Add lec01/findLongestSubstring.js

---
+++ lec01/findLongestSubstring.js
@@ -0,0 +1,27 @@
+function findLongestSubstring(str) {
+  let maxLength = 0;
+  let start = 0;
+  let seen = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    maxLength = Math.max(maxLength, i - start + 1);
+
+    seen[char] = i + 1;
+  }
+
+  return maxLength;
+}
+
+console.log(findLongestSubstring('')); // 0
+console.log(findLongestSubstring('rithmschool')); // 7
+console.log(findLongestSubstring('thisisawesome')); // 6
+console.log(findLongestSubstring('thecatinthehat')); // 7
+console.log(findLongestSubstring('bbbbbb')); // 1
+console.log(findLongestSubstring('longestsubstring')); // 8
+console.log(findLongestSubstring('thisishowwedoit')); // 6
