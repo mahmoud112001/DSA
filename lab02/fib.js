59ff5b35d53bea4dbdb801b53c4a261a937fb20a
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-06-14 20:10:57 +0200

    chore: update fib.js

--- lab02/fib.js
+++ lab02/fib.js
@@ -18,6 +18,7 @@ function fibIterative(n) {
 function fibMemo(n, memo = {}) {
   if (n <= 1) return n;
   if (memo[n]) return memo[n];
+  
   return (memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo));
 }
 
