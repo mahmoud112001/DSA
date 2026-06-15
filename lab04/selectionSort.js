a50da78e0fa22bf46fd1164439e4905b6c362fec
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-06-15 22:05:49 +0200

    chore: update selectionSort.js

--- lab04/selectionSort.js
+++ lab04/selectionSort.js
@@ -26,5 +26,4 @@ function selectionSort(arr) {
 const unsorted = [64, 25, 12, 22, 11];
 console.log("Input: ", [...unsorted]);
 console.log("Sorted:", selectionSort(unsorted));
-
 module.exports = { selectionSort };
