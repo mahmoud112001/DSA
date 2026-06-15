88289d2806dd9b8efd90f978401d08ca2aff2d30
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-06-15 22:05:51 +0200

    chore: update maxSubarraySum.js

--- lec01/maxSubarraySum.js
+++ lec01/maxSubarraySum.js
@@ -8,10 +8,7 @@ function maxSubarraySum(arr, num) {
   for (let i = 0; i < num; i++) {
     maxSum += arr[i];
   }
-
   tempSum = maxSum;
-
-  
   for (let i = num; i < arr.length; i++) {
     tempSum = tempSum - arr[i - num] + arr[i];
     
